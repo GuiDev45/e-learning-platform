4a4ef46a2152c0443fc1ce42ea3376c114cf29c2
GuiDev45 <bortolettodev@gmail.com>
2024-09-08 18:01:09 -0300

    Esqueleto page login, bibliotecas externas, componente Input estilizado

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,52 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+// import App from "./App.tsx";
 import "./reset.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import CourseSchedule from "./pages/CourseSchedule/index.tsx";
+import Login from "./pages/Login/index.tsx";
+import Register from "./pages/Register/index.tsx";
+import Classroom from "./pages/Classroom/index.tsx";
+import LostPassword from "./pages/LostPassword/index.tsx";
+import AreaTeacher from "./pages/AreaTeacher/index.tsx";
+import NotFoundPage from "./pages/NotFoundPage/index.tsx";
+
+// ver a documentação do react router
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <CourseSchedule />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/classroom",
+    element: <Classroom />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/lostPassword",
+    element: <LostPassword />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/areaTeacher",
+    element: <AreaTeacher />,
+    errorElement: <NotFoundPage />,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
